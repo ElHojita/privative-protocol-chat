4fd54222dd108357547bf2456ace96159774fc97
Sergio Silvestre <sortega652@hotmail.com>
2019-08-22 03:00:16 -0600

    General, solving error of conection to server xmpp

--- src/pxmpp/Logic.java
+++ src/pxmpp/Logic.java
@@ -1,8 +1,12 @@
 package pxmpp;
+import java.util.Collection;
 import java.util.Scanner;
 import org.jivesoftware.smack.AccountManager;
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smackx.packet.VCard;
 /**
  * @author Silvestre
@@ -58,7 +62,7 @@ public class Logic  {
         catch(Exception ex)
         {
             //ex.printStackTrace();// this capture the exception, but in this case print the follow mensaje.
-              System.out.println("Server error or account already in server.");
+              System.out.println("Server error or account already in server, more details: " + ex.getMessage());
         }
     }
     
@@ -78,8 +82,8 @@ public class Logic  {
         }
         catch(Exception ex)
         {
-            System.out.println("Password incorrect or account does not exist.");
-            ex.printStackTrace();
+            System.out.println("Password incorrect or account does not exist, more details: " + ex.getMessage());
+            //ex.printStackTrace();
         }
     }
     
@@ -92,7 +96,7 @@ public class Logic  {
         catch(Exception ex)
         {
             //ex.printStackTrace();
-            System.out.println("Server error or session is already over.");
+            System.out.println("Server error or session is already over, more details: " + ex.getMessage());
         }
     }
      
@@ -105,7 +109,7 @@ public class Logic  {
             varConect.disconnect();
             varConect = null;
             
-            System.out.println("Account deleted successfully.");
+            System.out.println("Account deleted successfully, more details: " + ex.getMessage());
         }
         catch(Exception ex)
         {
@@ -115,7 +119,42 @@ public class Logic  {
        
     //end Adminstration, creation, login, logout and delete account
     
+     //begin  Show Users
      
+       public static void ShowUsers()
+    {
+        Roster roster = varConect.getRoster();
+        Collection<RosterEntry> entries = roster.getEntries();
+        System.out.println("User \t\t Status");
+        for (RosterEntry entry : entries) {
+            System.out.println(entry.getName() +  " t\t " + entry.getStatus());
+        }
+    }
+       
+        public static void addUsers()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("User id to add: ");
+        String lContacto = scan.nextLine();
+        
+        try
+        {
+            Roster.setDefaultSubscriptionMode(Roster.SubscriptionMode.manual);
+            Roster roster = varConect.getRoster();
+
+            if (!roster.contains(lContacto)) {
+                roster.createEntry(lContacto + "@alumchat.xyz", lContacto, new String[] { "Friends" });
+            } else {
+                System.out.println("User already in your list of friends.");
+            }
+        }
+        catch(XMPPException ex)
+        {
+            System.out.println("Account does exist or already in your list of friends, details: " + ex.getMessage());
+        }
+    }
+       
+     //end Show Users
     
     
 }// End Logic
