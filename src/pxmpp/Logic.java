c3c6e0f0a17156310849acba7d6c7fd1066bf932
Sergio Silvestre <sortega652@hotmail.com>
2019-08-22 01:51:54 -0600

    Logic, Creation of account in the server xmpp

--- src/pxmpp/Logic.java
+++ src/pxmpp/Logic.java
@@ -1,37 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pxmpp;
-
+import java.util.Scanner;
+import org.jivesoftware.smack.AccountManager;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
-
+import org.jivesoftware.smackx.packet.VCard;
 /**
- *
  * @author Silvestre
  */
-public class Logic {
+public class Logic  {
     public static ConnectionConfiguration varConfig = null;
     public static XMPPConnection varConect = null;   
     
-    
-    
-    
-    
-        public static void ConfiConection()
+    //begin Conection, configuration and status
+        public static void ConfiConection ()
     {
         varConfig = new ConnectionConfiguration("alumchat.xyz",5222);
         varConfig.setSecurityMode(ConnectionConfiguration.SecurityMode.disabled);
         varConfig.setDebuggerEnabled(false);
         varConfig.setSendPresence(true);
     }
-    
     public static boolean Isonline()
     {
         return (varConect != null && varConect.isConnected());   
     }
+    //end Conection, configuration and status
+    //begin Adminstration, creation, login, logout and delete account
+    public static void RegisterAccount()
+    {
+        try
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Name: ");
+            String lNombre = scan.nextLine();
+            System.out.print("last Name: ");
+            String lApellido = scan.nextLine();
+            System.out.print("User: ");
+            String lUsuario = scan.nextLine();
+            System.out.print("Password: ");
+            String lClave = scan.nextLine();
+            
+            varConect = new XMPPConnection(varConfig);
+            varConect.connect(); 
+            AccountManager lManager = varConect.getAccountManager();
+            lManager.createAccount(lUsuario,lClave);
+           varConect.login(lUsuario, lClave);
+            
+            VCard vcard = new VCard();
+            vcard.load(varConect, lUsuario + "@alumchat.xyz");
+            vcard.setFirstName(lNombre);
+            vcard.setLastName(lApellido);
+            vcard.setEmailHome(lUsuario + "@alumchat.xyz");
+            vcard.save(varConect);
+            
+            System.out.println("account successfully created.");
+        }
+        catch(Exception ex)
+        {
+            //ex.printStackTrace();// this capture the exception, but in this case print the follow mensaje.
+              System.out.println("Server error or account already in server.");
+        }
+    }
+    //end Adminstration, creation, login, logout and delete account
     
     
-}
+}// End Logic
